4a1e9197d5c13d0678f85779dc1f830e4196023a
Mike Lyons <mdl0394@gmail.com>
2013-04-19 02:10:40 -0400

    Initial Commit

---
+++ real_index.php
@@ -0,0 +1,93 @@
+<html>
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Shanti' rel='stylesheet' type='text/css'>
+	<link href='reset.css' rel='stylesheet' type='text/css'>
+	<link href='main.css' rel='stylesheet' type='text/css'>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
+	<script src="main_script.js"></script>
+</head>
+<body>
+	<div id="body">
+		<div id="menu_area">
+			<div class="title" id="name">
+				M<a id="i">i</a>ke Lyons  
+			</div>
+			<div id="menu">
+				<a class="nav_link nav_link_selected color2" id="aboutme">
+					About Me
+				</a>
+				<a class="nav_link" id="projects" href="construction.php">
+					Projects
+				</a>
+				<a class="nav_link" id="contact" href="construction.php">
+					Contact
+				</a>
+			</div>
+		</div>
+		<div id="content_area">
+			<div class="content" id="main_content">
+				<img src="images/me_cartoon.png" height="300px" style="float: right; padding-left: 5px;" />
+				My name is Mike Lyons, and I am a Software Engineering student at Rochester Institute of Technology. I am a dedicated and brilliant programmer who enjoys applying his programing knowledge to solve a variety of problems. I am currently looking for Coops or Internships as a software engineer.
+				<br/><br/>
+				So far in my career I have worked with many different programming languages, and have learned to adapty quickly to new developing enviroments and strategies. My main interests are in artificial intelligence and it's application to making human day to day life easier. But I am generally willing to program anything that seems fun or chalenging.
+			</div>
+			<div id="more_info">
+				<div class="content_menu" id="menu1">
+					<div class="content_menu_item content_menu_item_selected color2" id="technical">
+						<div class="content_menu_item_text">
+							Technical Skills
+						</div>
+					</div>
+					<div class="content_menu_item color2" id="2">
+						<div class="content_menu_item_text">
+							For Fun
+						</div>
+					</div>
+					<div class="content_menu_item color2" id="3">
+						<div class="content_menu_item_text">
+							Menu 3
+						</div>
+					</div>
+				</div>
+				<div class="content_box color2" id="menu1_box">
+					<div class="content content_box_item content_box_item_selected" id="technical_box">
+						I have technical knowledge in the following programming languages:
+						<ul class="content_list">
+							<li>Objective C with Cocoa touch</li>
+							<li>Java</li>
+							<li>C++</li>
+							<li>Python</li>
+							<li>PHP, MySQL</li>
+							<li>HTML, Javascript (With jQuery)</li>
+							<li>Visual Basic</li>
+							<li>Bash scripting</li>
+							<li>Oracle SQL Introduction to 9i Certified</li>
+						</ul>
+					</div>
+
+					<div class="content content_box_item" id="2_box">
+						<img src="images/First-Logo.png" height="100px" style="float: left; padding-right: 5px;" />
+						During high school a large part of my inspiration has come from the FIRST Robotics program. My team was the Powerhawks FRC team 1111. I was the head of the programming team my senior year, and a large part of creating the website for the team. FIRST robotics is an amazing program that allows for high school kids to develop an idea of what it is like to build and deploy a product in a real world setting under a real world deadline.
+						<br/><br/>
+						<img src="images/sse.png" height="50px" style="float: right; padding-right: 5px;" />
+						So far during college a large part of my extra curricular activities have been involved with the RIT Society of Software engineers. I am a part of both the SSE Dodgeball team and the SSE Ultimate frisbee team.
+					</div>
+
+					<div class="content content_box_item" id="3_box">
+						3 content
+					</div>
+				</div>
+			</div>
+			<div id="footer">
+					Website created by and for Mike Lyons<br/>
+					Adiminstration
+			</div>
+		</div>
+		<div id="ibox" class="content">
+			Renee is awesome
+		</div>
+	</div>
+</body>
+</html>
